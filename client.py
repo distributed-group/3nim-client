092c75a1bde2d159c133f4ecf650ee29e85ab82e
Skorp7 <42909250+Skorp7@users.noreply.github.com>
2022-11-22 18:06:54 +0200

    greeting peers

--- client.py
+++ client.py
@@ -9,13 +9,14 @@ from jsonrpcclient import Ok, parse_json, request_json
 
 
 
-async def start_game(players):
-	async with websockets.connect("ws://"+players['player2']+":5002") as ws:        
-		req = request_json("validZipCode", {"zip":"12345"})
-		print(req) 
+async def start_game(players, my_ip):
+	#connect with player number 1
+	first_peer_ip = players['player1']
+	second_peer_ip = players['player2']
+	async with websockets.connect('ws://'+first_peer_ip+':5002') as ws:        
+		req = request_json('greet', {"message": 'Hello, I connected with you, my ip is ' + my_ip})
 		await ws.send(req)
 		result = await ws.recv()
-		print(result)  
 		response = parse_json(result)    
 	if isinstance(response, Ok):
 		print(response.result)
@@ -23,13 +24,8 @@ async def start_game(players):
 		logging.error(response.message)
 
 @method
-async def validZipCode(zip) -> Result:
-    if zip == "":
-        return InvalidParams("Null value")
-    if re.match("^[0-9]{5}(?:-[0-9]{4})?$", zip):
-        result = { "zip": zip, "result" : "Valid Zipcode" }
-    else:
-        result = { "zip": zip, "result" : "Invalid Zipcode" }
+async def greet(message) -> Result:
+    result = { "message": message, "result" : "Nice to hear you!" }
     return Success(result)
 
 async def start_socket(websocket, path):
@@ -38,11 +34,14 @@ async def start_socket(websocket, path):
         
 def connect():
 	server = Server('http://192.168.10.5:5001')
+	my_ip = socket.gethostbyname(socket.gethostname())
 	try:
-		response = server.want_to_play(socket.gethostbyname(socket.gethostname()))
+		response = server.want_to_play(my_ip)
+		print(response)
 		if response['status'] != 'wait for players':
-			asyncio.get_event_loop().run_until_complete(start_game(response))		
-		start_server = websockets.serve(start_socket, socket.gethostbyname(socket.gethostname()), 5002)
+			asyncio.get_event_loop().run_until_complete(start_game(response, my_ip))		
+		#start a websocket in port 5002 in own ip adress
+		start_server = websockets.serve(start_socket, my_ip, 5002)
 		asyncio.get_event_loop().run_until_complete(start_server)
 		asyncio.get_event_loop().run_forever()
 	except:
