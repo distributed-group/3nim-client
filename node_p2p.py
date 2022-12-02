e609778a6071bfe8c7d0da14866f15a02673f995
pinjaw <pinja.vierimaa@helsinki.fi>
2022-12-02 15:59:45 +0200

    Added dotenv and .gitignore.

--- node_p2p.py
+++ node_p2p.py
@@ -1,16 +1,20 @@
 import sys
+import os
 import time
 import socket
 from jsonrpclib import Server
 from NimPeerNode import NimPeerNode
+from dotenv import load_dotenv
+
+load_dotenv()
 
 my_ip = socket.gethostbyname(socket.gethostname())
 node = NimPeerNode(my_ip, 10001)
 
 def connect():
     #connect to json-rpc server
-    server_ip = '' #fill correct server ip here
-    server = Server('http://' +server_ip+ ':5001')
+    server_ip = os.getenv("SERVER_IP")
+    server = Server('http://' + server_ip + ':5001')
     try:
         response = server.want_to_play(my_ip)
         print(response)
